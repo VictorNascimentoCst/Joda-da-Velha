2f39e2e46f29dc0d56f5720182652fba09141a5f
Victor Nascimento <nascimento-77@hotmail.com>
2022-06-27 12:28:45 -0300

    Add the X and O simbol's event to the game

---
+++ index.js
@@ -0,0 +1,18 @@
+class jogo {
+    constructor() {
+        this.regulador = x
+    }
+}
+
+function show(slot) {
+    var id = slot.id
+    var y = document.getElementById(id)
+    if(this.regulador == 'x') {
+        y.setAttribute("class", "slot simboloX") 
+        this.regulador = "o"
+    } else {
+        y.setAttribute("class", "slot simboloO") 
+        this.regulador = "x"
+    }
+   
+}
